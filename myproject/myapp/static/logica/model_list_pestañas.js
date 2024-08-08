2c35a4a31b5484626aac7d0ce2fc9380a9c03706
Crisapps99 <cris.boy99@outlook.com>
2024-08-08 16:37:01 -0500

    estilos

--- myproject/myapp/static/logica/model_list_pestañas.js
+++ myproject/myapp/static/logica/model_list_pestañas.js
@@ -1,3 +1,4 @@
+//manejo de las pestañas 
 document.querySelectorAll('.menu p').forEach(button =>{
     button.addEventListener('click', () => {
 
@@ -15,4 +16,44 @@ document.querySelectorAll('.menu p').forEach(button =>{
         document.querySelector(targetId).classList.add('active');
     }) 
 })
+//guardamos las pestañas 
+document.querySelectorAll('.menu p').forEach(tab =>{
+    tab.addEventListener('click', function(){
+        const target = this.getAttribute('data-target');
+
+        document.querySelectorAll('.tab-content').forEach(content =>{
+            content.classList.remove('active');
+        });
+
+        document.querySelector(target).classList.add('active');
+        sessionStorage.setItem('activeTab',target);
+    });
+});
+
+//restauramos la pestaña activa al cargar la pagina 
+document.addEventListener('DOMContentLoaded', function(){
+    //obtengo el id
+    const activeTab = sessionStorage.getItem('activeTab');
+    if(activeTab){
+        //elimino la clase activa de todas las pestañas 
+        document.querySelectorAll('.tab-content').forEach(content =>{
+            content.classList.remove('active');
+        });
+
+    //agrego la calse activa al contenido de la pestaña activa 
+    document.querySelector(activeTab).classList.add('active');
+
+    //marco la pestaña activa y als demas inactivas
+    document.querySelectorAll('.menu p').forEach(tab => {
+        if (tab.getAttribute('data-target') === activeTab){
+            tab.classList.remove('inactive');
+            tab.classList.add('active');
+        }else{
+            //si no esta activa se le agrega la calse inactive
+            tab.classList.remove('active');
+            tab.classList.add('inactive');
+        }
+    });
+}
+});
 
